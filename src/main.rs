f9959058af353377c09cf317a253bf5a54e71ffd
sinyo-matu <sinyo_matu@icloud.com>
2022-04-03 17:24:12 +0900

    update:add data connection info

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ use zero2prod::{
 
 #[tokio::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // init logger
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
     let configuration = get_configuration();
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
-    // init logger
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
     let listener = TcpListener::bind(format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
